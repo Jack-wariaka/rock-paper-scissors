abe151149cc882924188a2479d57c4b68bfe077a
jack-wariaka <jackwariaka@gmail.com>
2026-07-09 23:05:34 +0300

    added the play game and play round logic

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let humanScore = 0;
+let computerScore = 0;
 function getHumanChoice(){
     let choice;
     alert('this is a rock paper, scissors game make your choice');
@@ -5,35 +7,52 @@ function getHumanChoice(){
     if (choice) {
         choice = choice.toLowerCase();
     } else {
-        console.log('please enter a valid choice')
+        alert('please enter a valid choice')
     }
     let isValid;
     isValid = choice === "rock" || choice === "paper" || choice === "scissors";
     if (isValid) {
         return choice;
     } else{
-        return("please enter a valid choice");
+        alert("please enter a valid choice");
     }
 }
 
 function getComputerChoice() {
     const min = 1;
-    const max = 4;
-    const num  = Math.floor(Math.random() * (max - min)) + min;
-    return(num);
+    const max = 3;
+    const num  = Math.floor(Math.random() * (max - min + 1)) + min;
+    switch (num) {
+        case 1:
+            return "rock";
+        case 2:
+            return "paper";
+        case 3:
+            return "scissors";
+    }
 }
 
-function test(num){
-    let count = 0;
-    let randomNum;
-    while (randomNum != num){
-        randomNum = getComputerChoice();
-        count++;
-        if (count ===  10000000){
-            console.log("impossible");
-            break;
-        }
-    }
-    return count;
+function playRound(){
+    const computerChoice = getComputerChoice();
+    const humanChoice = getHumanChoice();
+    if (humanChoice === computerChoice) {
+        alert(`its a tie you both chose ${humanChoice}`);
+    } else if (humanChoice === "rock" && computerChoice === "scissors" |
+               humanChoice === "paper" && computerChoice === "rock" |
+               humanChoice === "scissors" && computerChoice === "paper"){
+                alert(`you win! ${humanChoice} beats ${computerChoice}`);
+                humanScore++;
+               }else {
+                alert(`computer wins ${computerChoice} beats ${humanChoice}`);
+                computerScore++;
+               }
 }
-console.log(test(4));
+function playGame(times){
+    while (times > 0){
+        playRound();
+        times--;
+    }
+    alert(`your score is ${humanScore} \n
+        computer score is ${computerScore}`);
+}
+playGame(3);
