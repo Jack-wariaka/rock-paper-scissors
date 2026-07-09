79a79dbd85437ecf41f459420b4eed8ae2d09188
majack <jackwariaka@gmail.com>
2026-07-09 21:35:10 +0300

    added get computerchoice function

--- script.js
+++ script.js
@@ -16,5 +16,24 @@ function getHumanChoice(){
     }
 }
 
-let choice = getHumanChoice();
-alert(choice);
+function getComputerChoice() {
+    const min = 1;
+    const max = 4;
+    const num  = Math.floor(Math.random() * (max - min)) + min;
+    return(num);
+}
+
+function test(num){
+    let count = 0;
+    let randomNum;
+    while (randomNum != num){
+        randomNum = getComputerChoice();
+        count++;
+        if (count ===  10000000){
+            console.log("impossible");
+            break;
+        }
+    }
+    return count;
+}
+console.log(test(4));
